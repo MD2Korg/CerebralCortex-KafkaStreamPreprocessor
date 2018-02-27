db594ddd7d700e1abe0bdb64ee815c8756123590
ans25@buffalo.edu <ans25@buffalo.edu>
2018-02-27 17:30:36 +0000

    updated to be compatible with HDFS

--- run_vagrant.sh
+++ run_vagrant.sh
@@ -2,15 +2,13 @@
 
 # Python3 path
 export PYSPARK_PYTHON=/usr/bin/python3.6
-
+export LD_LIBRARY_PATH=/home/vagrant/hadoop/lib/native/
 # add current project root directory to pythonpath
 #export PYTHONPATH="${PYTHONPATH}:/cerebralcortex/code/CerebralCortex/"
 
 #Spark path
 export SPARK_HOME=/usr/local/spark/
 
-#PySpark args (do not change unless you know what you are doing)
-#export PYSPARK_SUBMIT_ARGS="--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.0 pyspark-shell"
 
 #set spark home
 export PATH=$SPARK_HOME/bin:$PATH
@@ -28,12 +26,10 @@ BATCH_DURATION="10"
 KAFKA_BROKER="127.0.0.1:9092"
 
 # spark master
-SPARK_MASTER="local[*]"
+SPARK_MASTER="local[2]"
 
-PY_FILES="/home/vagrant/CerebralCortex/dist/MD2K_Cerebral_Cortex-2.0.0-py3.6.egg,dist/MD2K_Cerebral_Cortex_Kafka_File_Queue_Processor-2.1.0-py3.6.egg"
 
 spark-submit --master $SPARK_MASTER --packages \
-org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.0 --py-files $PY_FILES \
+org.apache.spark:spark-streaming-kafka-0-8_2.11:2.2.0 \
 main.py -c $CC_CONFIG_FILEPATH -d $DATA_DIR -b$KAFKA_BROKER -bd $BATCH_DURATION
-
 
